77ee26f8fef1b5a79be0fe8f206ef740ab97d060
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-05-27 18:25:46 +0100

    Initial Commit

---
+++ BoostSDK/BoostSDK.h
@@ -0,0 +1,19 @@
+//
+//  BoostSDK.h
+//  BoostSDK
+//
+//  Created by Ondrej Rafaj on 27/05/2018.
+//  Copyright © 2018 Ford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BoostSDK.
+FOUNDATION_EXPORT double BoostSDKVersionNumber;
+
+//! Project version string for BoostSDK.
+FOUNDATION_EXPORT const unsigned char BoostSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoostSDK/PublicHeader.h>
+
+
